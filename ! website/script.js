4845929aa1ba9c962c69ce4d6c40226f9318d348
remysedlak <remysedlak@gmail.com>
2025-03-16 14:53:04 -0400

    Add preloader functionality and restructure index.html; update stylesheet.css for new styles

---
+++ ! website/script.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", function () {
+    document.getElementById("preloader").style.display = "none";
+    document.getElementById("content").classList.remove("hidden");
+});
